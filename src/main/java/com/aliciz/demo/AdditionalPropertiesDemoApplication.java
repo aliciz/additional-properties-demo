7c30bf21d1dbbab0118fad4c3f13cb84c4a8ae7b
Lsh <lsh1990826@gmail.com>
2021-09-25 23:36:27 +0800

    first commit

---
+++ src/main/java/com/aliciz/demo/AdditionalPropertiesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.aliciz.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdditionalPropertiesDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdditionalPropertiesDemoApplication.class, args);
+    }
+
+}
